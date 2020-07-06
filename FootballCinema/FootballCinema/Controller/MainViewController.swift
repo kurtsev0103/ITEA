d58ce6ed21a0c50971d72c58b7c9ce43181eae6f
kurtsev0103 <kurtsev0103s@gmail.com>
2020-07-06 18:32:40 +0200

    add check orientation

--- FootballCinema/FootballCinema/Controller/MainViewController.swift
+++ FootballCinema/FootballCinema/Controller/MainViewController.swift
@@ -129,8 +129,17 @@ extension MainViewController {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.4))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        let groupSize: NSCollectionLayoutSize
+        let group: NSCollectionLayoutGroup
+        
+        if UIDevice.current.orientation.isLandscape {
+            groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.4))
+            group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        } else {
+            groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.8))
+            group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        }
+
         let spacing = CGFloat(15)
         group.interItemSpacing = .fixed(spacing)
         
